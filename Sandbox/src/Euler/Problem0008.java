4c4debc325236b1f796f276bf0e4b88b6d289abc
simonsez1808 <simonboulton@gmail.com>
2013-08-07 22:26:16 +0100

    xxzcxzcx

--- Sandbox/src/Euler/Problem0008.java
+++ Sandbox/src/Euler/Problem0008.java
@@ -1,5 +1,5 @@
 package Euler;
-
+///////////////////////////////////
 
 /*Find the greatest product of five consecutive digits in the 1000-digit number.
 73167176531330624919225119674426574742355349194934
