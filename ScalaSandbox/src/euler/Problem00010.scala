5d10ddb6247e6c9b6b1a6574306878f56185cc2c
simonsez1808 <simonboulton@gmail.com>
2013-08-13 16:35:38 +0100

    Signed-off-by: simonsez1808 <simonboulton@gmail.com>

--- ScalaSandbox/src/euler/Problem00010.scala
+++ ScalaSandbox/src/euler/Problem00010.scala
@@ -1,16 +1,15 @@
 package euler
 
-/*
- * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
- * 
- * Find the sum of all the primes below two million.
-*/
+
 object Problem00010 {
 
-  def main(args: Array[String]): Unit = {}
-  
-  println("Problem00010");
+  def main(args: Array[String]): Unit = {
+    
+      println("Problem00010");
   
   println("fred")
+  }
+  
+
 
 }
