e18f399e9184ade9b214e1587fbce25fa75897ec
simonsez1808 <simonboulton@gmail.com>
2013-08-16 21:20:26 +0100

    Signed-off-by: simonsez1808 <simonboulton@gmail.com>

---
+++ Sandbox/src/euler/Problem0015.java
@@ -0,0 +1,51 @@
+package euler;
+
+import java.math.BigInteger;
+
+/*Starting in the top left corner of a 2×2 grid, and only being able
+ * to move to the right and down, there are exactly 6 routes to the bottom
+ * right corner. 
+ * 
+ * How many such routes are there through a 20×20 grid?*/
+
+public class Problem0015 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i < 21; i++) {
+			System.out.println("Side length = " + i + ": "
+					+ new Grid(i).getPaths());
+		}
+	}
+
+	static class Grid {
+		int n;
+
+		Grid(int lengthOfSide) {
+			n = lengthOfSide;
+		}
+
+		BigInteger getPaths() {
+			BigInteger term1 = factorial(2 * n);
+			BigInteger term2 = factorial(n).multiply(factorial(n));
+		
+			return term1.divide(term2);
+		}
+
+		BigInteger factorial(int n) {
+			BigInteger result = new BigInteger("0");
+			System.out.println("Factorial of " + n);
+			if (n == 1) {
+				return new BigInteger("1");
+			} else {
+				System.out.println("Result = " + result);
+				result.add(factorial(n - 1).multiply(new BigInteger(Integer.toString(n))));
+				System.out.println("Result = " + result);
+				return result;
+			}
+		}
+
+	}
+
+}
