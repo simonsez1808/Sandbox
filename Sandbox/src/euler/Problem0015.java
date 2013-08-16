542532d13a720f7fa99735ef4a979962f09342d7
simonsez1808 <simonboulton@gmail.com>
2013-08-16 23:02:18 +0100

    Signed-off-by: simonsez1808 <simonboulton@gmail.com>

--- Sandbox/src/euler/Problem0015.java
+++ Sandbox/src/euler/Problem0015.java
@@ -28,20 +28,21 @@ public class Problem0015 {
 
 		BigInteger getPaths() {
 			BigInteger term1 = factorial(2 * n);
+			System.out.println("Term1: " + term1);
 			BigInteger term2 = factorial(n).multiply(factorial(n));
-		
+			System.out.println("Term2: " + term2);
 			return term1.divide(term2);
 		}
 
 		BigInteger factorial(int n) {
 			BigInteger result = new BigInteger("0");
-			System.out.println("Factorial of " + n);
-			if (n == 1) {
-				return new BigInteger("1");
+//			System.out.println("Factorial of " + n);
+			if (n == 1 || n == 2) {
+				return new BigInteger(String.valueOf(n));
 			} else {
-				System.out.println("Result = " + result);
+//				System.out.println("Result = " + result);
 				result.add(factorial(n - 1).multiply(new BigInteger(Integer.toString(n))));
-				System.out.println("Result = " + result);
+//				System.out.println("Result = " + result);
 				return result;
 			}
 		}
