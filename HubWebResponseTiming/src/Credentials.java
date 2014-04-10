bbb9882a689125398cad096a5a30856ad794f36c
simonb <simonb@PC1309Simon.practiceplan.local>
2014-04-10 14:05:55 +0100

    c5

--- HubWebResponseTiming/src/Credentials.java
+++ HubWebResponseTiming/src/Credentials.java
@@ -1,4 +1,5 @@
 public class Credentials {
 	public static final String userName = "itadmin@practiceplan.co.uk";
 	public static final String password = "Hwey38dbYqwj";
+	public static final String WEBSITE_ADDRESS = "https://www.practiceplan.co.uk";
 }
