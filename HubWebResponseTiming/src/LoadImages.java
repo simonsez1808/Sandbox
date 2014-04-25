0ca433e5020a6cd8fdb7645e4ae58fe06ed79ed9
simonsez1808 <simonboulton@gmail.com>
2014-04-25 08:45:40 +0100

    c1

--- HubWebResponseTiming/src/LoadImages.java
+++ HubWebResponseTiming/src/LoadImages.java
@@ -41,45 +41,58 @@ public class LoadImages {
 
 			Thread.sleep(INTERVAL_BETWEEN_EACH_TEST_CYCLE_SECS * 1000);
 		}
-		
+
 	}
 
 	private static void runTest() throws IOException, MalformedURLException {
-		long start = System.nanoTime();
-		// TODO Auto-generated method stub
-		final WebClient webClient = new WebClient();
-		System.out.println("Web client timeout is set to "
-				+ webClient.getOptions().getTimeout());
-		webClient.getOptions().setJavaScriptEnabled(false);
-		System.out.println("Loading main page");
-		HtmlPage page0 = webClient
-				.getPage("https://www.practiceplan.co.uk/home");
-		System.out.println("Page loaded");
-		DomNodeList dml = page0.getElementsByTagName("img");
-		System.out.println(dml.getLength());
-
-		Iterator<HtmlImage> it = dml.iterator();
-
-		while (it.hasNext()) {
-			HtmlImage image = it.next();
-			System.out.println("Source: " + image.getAttribute("src"));
-			BufferedImage loadedImage = ImageIO.read(new URL(
-					"https://www.practiceplan.co.uk/web.nsf/"
-							+ image.getAttribute("src")));
-		}
-
-		System.out.println("Completed " + (double) (System.nanoTime() - start)
-				/ 1000000000);
 
 		FileWriter output = new FileWriter(
 				"c:/users/simonb/desktop/testwebtimer.txt", APPEND_TO_FILE);
 		BufferedWriter writer = new BufferedWriter(output);
-		Calendar cal = Calendar.getInstance();
-		writer.write(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(cal
-				.getTime())
-				+ "|"
-				+ String.valueOf((double) (System.nanoTime() - start) / 1000000000));
-		writer.newLine();
+		long start = System.nanoTime();
+		try {
+
+			// TODO Auto-generated method stub
+			final WebClient webClient = new WebClient();
+			System.out.println("Web client timeout is set to "
+					+ webClient.getOptions().getTimeout());
+			webClient.getOptions().setJavaScriptEnabled(false);
+			System.out.println("Loading main page");
+			HtmlPage page0 = webClient
+					.getPage("https://www.practiceplan.co.uk/home");
+			System.out.println("Page loaded");
+			DomNodeList dml = page0.getElementsByTagName("img");
+			System.out.println(dml.getLength());
+
+			Iterator<HtmlImage> it = dml.iterator();
+
+			while (it.hasNext()) {
+				HtmlImage image = it.next();
+				System.out.println("Source: " + image.getAttribute("src"));
+				BufferedImage loadedImage = ImageIO.read(new URL(
+						"https://www.practiceplan.co.uk/web.nsf/"
+								+ image.getAttribute("src")));
+			}
+
+			System.out.println("Completed "
+					+ (double) (System.nanoTime() - start) / 1000000000);
+
+			Calendar cal = Calendar.getInstance();
+			writer.write(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+					.format(cal.getTime())
+					+ "|"
+					+ String.valueOf((double) (System.nanoTime() - start) / 1000000000));
+			writer.newLine();
+		} catch (Exception e) {
+			System.out.println("Timed out");
+			Calendar cal = Calendar.getInstance();
+			writer.write(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+					.format(cal.getTime())
+					+ "|"
+					+ "Timed out");
+			writer.newLine();
+		}
+
 		writer.flush();
 		output.close();
 	}
