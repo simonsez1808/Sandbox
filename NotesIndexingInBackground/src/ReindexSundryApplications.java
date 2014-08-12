bf609b7798f16afdabc804f0d5a9735d961fb5d7
simonsez1808 <simonboulton@gmail.com>
2014-08-12 13:29:13 +0100

    General refactoring

--- NotesIndexingInBackground/src/ReindexSundryApplications.java
+++ NotesIndexingInBackground/src/ReindexSundryApplications.java
@@ -10,16 +10,30 @@ import lotus.domino.*;
 public class ReindexSundryApplications extends NotesThread {
 
 	static Session session;
+	static final String LOCAL_SERVER = "";
+
 	static String applicationNames[][] = {
+
+			// Auddis
 			{ "NDCOLORADO", "PARAOCT13\\pp_auddis.nsf" },
-			{ "", "PARAOCT13\\pp_auddis.nsf" },
+			{ LOCAL_SERVER, "PARAOCT13\\pp_auddis.nsf" },
+			// PP_Admin
 			{ "NDGEMINI", "2009System1\\pp_admin.nsf" },
-			{ "", "PARAJAN14\\pp_contacts.nsf" },
+			{ "NDDSVRM", "2009System1\\pp_admin.nsf" },
+			// Contacts
+			{ LOCAL_SERVER, "PARAJAN14\\pp_contacts.nsf" },
 			{ "NDCOLORADO", "PARAJAN14\\pp_contacts.nsf" },
-			{ "", "PARAJAN14\\pp_patients.nsf" },
+			{ LOCAL_SERVER, "PARAJAN14\\pp_contacts.nsf" },
+			{ "NDCOLORADO", "PARAJAN14\\pp_contacts.nsf" },
+			{ "NDGEMINI", "2009SYSTEM1\\pp_contacts.nsf" },
+			{ "NDDSVRM", "2009SYSTEM1\\pp_contacts.nsf" },
+			// Patients
+			{ LOCAL_SERVER, "PARAJAN14\\pp_patients.nsf" },
 			{ "NDCOLORADO", "PARAJAN14\\pp_patients.nsf" },
-			{ "", "PARAJAN14\\pp_contacts.nsf" },
-			{ "NDCOLORADO", "PARAJAN14\\pp_contacts.nsf" } };
+			{ LOCAL_SERVER, "PARAOCT13\\pp_patients.nsf" },
+			{ "NDCOLORADO", "PARAOCT13\\pp_patients.nsf" },
+			{ "NDGEMINI", "2009SYSTEM1\\pp_patients.nsf" },
+			{ "NDDSVRM", "2009SYSTEM1\\pp_patients.nsf" }, };
 
 	static boolean isQuiet;
 
@@ -38,7 +52,6 @@ public class ReindexSundryApplications extends NotesThread {
 
 			NotesThread.sinitThread();
 
-			// Get hold of Colorado
 			System.out.println("Creating session with automatic logon.");
 			session = NotesFactory.createSession((String) null, (String) null,
 					Credentials.PASSWORD);
@@ -46,8 +59,14 @@ public class ReindexSundryApplications extends NotesThread {
 			System.out.println(session.getCommonUserName());
 
 			for (int i = 0; i < applicationNames.length; i++) {
-				reindexNotesApplication(applicationNames[i][0],
-						applicationNames[i][1]);
+				
+				try {
+					reindexNotesApplication(applicationNames[i][0],
+							applicationNames[i][1]);
+				} catch (Exception e) {
+					System.out.println("ERROR: " + e.getMessage());
+				}
+				
 			}
 
 		} catch (Exception e) {
@@ -80,8 +99,9 @@ public class ReindexSundryApplications extends NotesThread {
 			View view = allViews.get(i);
 
 			if (!isQuiet) {
-				System.out.println("Starting to index view # " + (i + 1) + " of " + allViews.size() + " named "
-						+ view.getName());
+				System.out
+						.println("Starting to index view # " + (i + 1) + " of "
+								+ allViews.size() + " named " + view.getName());
 				// System.out.println("View has " + view.getEntryCount()
 				// + " view entries.");
 
@@ -92,11 +112,14 @@ public class ReindexSundryApplications extends NotesThread {
 				long durationSecs = (endIndex - startIndex) / 1000;
 				long minutes = durationSecs / 60;
 				long secs = durationSecs % 60;
-				
+
 				if (!isQuiet) {
 					System.out.println("Completed indexing view "
-							+ view.getName() + ", time taken = " + minutes
-							+ " mins " + secs + " seconds ");
+							+ view.getName()
+							+ ", time taken = "
+							+ (minutes > 0 ? minutes + " min"
+									+ (minutes > 1 ? "s " : " ") : "") + secs
+							+ " seconds ");
 				}
 
 			}
