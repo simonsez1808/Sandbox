03d283f16e76442df73e585208f0c51037470f5a
simonsez1808 <simonboulton@gmail.com>
2014-08-28 16:49:33 +0100

    sdsds

--- testproject1/src/Utils.java
+++ testproject1/src/Utils.java
@@ -2,7 +2,9 @@ public class Utils {
 
 	public static final String getDurationAsText(long startTimeMillisecs,
 			long endTimeMillisecs) {
-		long durationSecs = (endTimeMillisecs - startTimeMillisecs) / 1000;
+		
+		long durationMillisecs = endTimeMillisecs - startTimeMillisecs;	
+		long durationSecs = durationMillisecs / 1000;
 		long minutes = durationSecs / 60;
 		long secs = durationSecs % 60;
 		
