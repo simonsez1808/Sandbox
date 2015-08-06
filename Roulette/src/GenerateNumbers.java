6b57a95eb3f0a283acd2aa97f4b13ef3ae6afa3f
simonsez1808 <simonboulton@gmail.com>
2015-08-06 17:30:11 +0100

    new

--- Roulette/src/GenerateNumbers.java
+++ Roulette/src/GenerateNumbers.java
@@ -1,5 +1,15 @@
+// It's important that we make the changes as subtle as possible. Therefore, I've chosen to
+// use a pattern-matching approach based on the colour. If you get a certain
+// sequence of colours (and this can be as complicated as you like, then the
+// patterniser will guarantee that the next colour is predictable.
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Random;
+import java.util.Set;
 
 import static java.lang.System.out;
 
@@ -52,7 +62,62 @@ public class GenerateNumbers {
 }
 
 class Patterniser {
+private static final long MAX_BUFFER_LENGTH = 128;
+	Queue<Number> nudgedNumbers;
+	Map<String, String> patterns;
+	StringBuilder buffer;
 
 	Patterniser() {
+		patterns.put("RBRB", "B");
+		patterns.put("BRBR", "R");
+		nudgedNumbers = new LinkedList<Number>();
+	}
+
+	//if the last n characters of the buffer match, then create a new 
+	// random and save the last one to spit out later
+	Number process(Number number){
+		
+		// If the buffer's full, then take off the first char
+		if (buffer.length() == MAX_BUFFER_LENGTH) {
+			buffer.deleteCharAt(0);
+		}
+		
+		// Save the colour found
+		buffer.append(number.getColour());
+		
+		// Now see if we have a match with the tail of the buffer
+		for (Map.Entry<String, String> x : patterns.entrySet()) {
+			if (buffer.toString().endsWith(x.getKey())) {
+				// Match. Add the number to the queue
+				
+			}
+		}
+		
+	}
+	void pop(){}
+	void push(){}
+
+}
+
+class Number {
+	String number;
+
+	Number(String number) {
+		this.number = number;
+	}
+
+	String getColour() {
+		Integer number;
+		number = Integer.parseInt(this.number);
+
+		if (number == 0) {
+			return "G";
+		}
+
+		if (number % 2 == 0) {
+			return "B";
+		}
+
+		return "R";
 	}
 }
