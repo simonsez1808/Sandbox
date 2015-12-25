2a4c15b1da2d6eb09b435531248780db81e317a3
Simon <simonboulton@gmail.com>
2015-12-25 23:48:18 +0000

    Revert "Revert "test""
    This reverts commit ff8b9140399fb81ef54a5b1b3723cc33984004a2.

--- Euler/src/euler/Problem0018.java
+++ Euler/src/euler/Problem0018.java
@@ -63,6 +63,7 @@ public class Problem0018 {
 		List<List<Integer>> pyramid = new ArrayList<List<Integer>>();
 		pyramid = setup();
 		Integer previousHighestIndex = null;
+		
 		for (int rowNum = 0; rowNum < pyramid.size(); rowNum++) {
 			
 			List<Integer> row = new ArrayList<Integer>(pyramid.get(rowNum));
@@ -84,9 +85,8 @@ public class Problem0018 {
 		// the next one
 		if (row.get(previousHighestIndex) > row.get(previousHighestIndex + 1)) {
 			return previousHighestIndex;
-		} else {
-			return previousHighestIndex + 1;
 		}
+			return previousHighestIndex + 1;
 	}
 
 	private static List<List<Integer>> setup() {
