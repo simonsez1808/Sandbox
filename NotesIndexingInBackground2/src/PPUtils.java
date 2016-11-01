6ff6a9966c0c58c3561daa3f954b819641fc49c9
simonsez1808 <simonboulton@gmail.com>
2016-11-01 08:55:15 +0000

    Updated PPUtils

--- NotesIndexingInBackground2/src/PPUtils.java
+++ NotesIndexingInBackground2/src/PPUtils.java
@@ -1,4 +1,36 @@
+import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.NotesException;
+import lotus.domino.NotesFactory;
+import lotus.domino.Session;
 
 public class PPUtils {
 
+	static Database thisDb;
+	static Session session;
+
+	static {
+		try {
+			session = NotesFactory.createSession((String) null, (String) null, Authentication.getInfo());
+			thisDb = session.getCurrentDatabase();
+
+		} catch (NotesException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param sendTo
+	 * @param subject
+	 * @param body
+	 * @throws NotesException
+	 */
+	public static void sendSimpleEmail(String sendTo, String subject, String body) throws NotesException {
+		Document mailDoc = thisDb.createDocument();
+		mailDoc.replaceItemValue("SendTo", sendTo.split(","));
+		mailDoc.replaceItemValue("Subject", subject);
+		mailDoc.replaceItemValue("body", body);
+		mailDoc.send();
+	}
 }
