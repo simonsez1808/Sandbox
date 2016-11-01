20aaf9455d374c909a616a405b06b2623bed939d
simonsez1808 <simonboulton@gmail.com>
2016-11-01 17:57:34 +0000

    Final day

--- FunctionalProgramming/src/Factorial.java
+++ FunctionalProgramming/src/Factorial.java
@@ -1,12 +1,11 @@
 
 public class Factorial {
 
-}int myFactorial( int integer)
-{
-if( integer == 1)
-     return 1;
-else
-       {
-       return(integer*(myFactorial(integer-1);
-       }
+	int myFactorial(int integer) {
+		if (integer == 1) {
+			return 1;
+		} else {
+			return (integer * (myFactorial(integer - 1)));
+		}
+	}
 }
