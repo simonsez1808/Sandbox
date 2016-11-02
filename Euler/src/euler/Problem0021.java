7ce283fd3c2f687fd890315aa702aa468204aab1
simonsez1808 <simonboulton@gmail.com>
2016-11-02 08:32:41 +0000

    Checked code

--- Euler/src/euler/Problem0021.java
+++ Euler/src/euler/Problem0021.java
@@ -18,9 +18,10 @@ public class Problem0021 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 1000; i++) {
 			Number n = new Number(i);
 			System.out.println(i + ": " + n.properDivisors + " Sum: " + n.sumOfProperDivisors);
+			
 		}
 	}
 	
